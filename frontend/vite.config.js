788f04a496bfc9e72daf75f6efbde9c608be9faa
Philipp Langer <philipplanger@yahoo.com>
2025-07-04 09:36:27 +0200

    Fix import paths in ContentManageView.vue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  extensions: ['.js', '.ts', '.vue', '.json'],
   server: {
     host: '0.0.0.0',
     port: 3000,
