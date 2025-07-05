25f846df75c56a32f68294a01f13d6d0c70c1a7e
Philipp Langer <philipplanger@yahoo.com>
2025-07-05 02:06:50 +0200

    Remove unguarded PRAGMA in KeywordsConfig.ready()

--- backend/keywords/apps.py
+++ backend/keywords/apps.py
@@ -6,10 +6,10 @@ class KeywordsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'keywords'
 
-    def ready(self):
+def ready(self):
         conn = connections['default']
         engine = conn.settings_dict.get('ENGINE', '')
-        # nur bei SQLite das WAL-Pragma setzen
+         # nur bei SQLite das WAL-Pragma setzen
         if 'sqlite3' in engine:
-            with conn.cursor() as cursor:
-                cursor.execute('PRAGMA journal_mode=WAL;')
+             with conn.cursor() as cursor:
+                 cursor.execute('PRAGMA journal_mode=WAL;')
