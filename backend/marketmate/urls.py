c2cb7ed732ab70cd7f16a9540a1a4d851c823e36
Philipp Langer <philipplanger@yahoo.com>
2025-07-06 11:44:16 +0200

    Add health check endpoints

--- backend/marketmate/urls.py
+++ backend/marketmate/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', lambda req: JsonResponse({'status': 'ok'})),
+    path('api/health/', lambda req: JsonResponse({'status': 'ok'})),
     path('api/users/',    include('users.urls')),
     path('api/websites/', include('websites.urls')),
     path('api/keywords/', include('keywords.urls')),
